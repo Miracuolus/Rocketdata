ecbf9b0d0627fa611bda8f499950e90b36a98f08
Vika Ratomskaya <ratom_v@mail.ru>
2020-10-17 19:12:18 +0300

    Parse data in the scripts

--- address_collector.py
+++ address_collector.py
@@ -18,13 +18,34 @@ def create_report(file_name, value):
 
 
 def collector(url):
+    new_data = []
     s = requests.get(url)
     if (s.status_code == 200):
         soup = BeautifulSoup(s.text)
-        city = soup.findAll('div', class_='city-item')
-        print(city)
-        create_report('json.txt', city)
-        
+        group = soup.findAll('div', {'class': 'city-item'})
+        #print(city)
+        create_report('json.txt', group)
+        for item in group:
+
+            # getting movie_id
+            city = item.find('div', {'class': 'expand-block-header js-ex-btn'}).find('h4').text
+            print(city)
+            shop_name = item.find('div', {'class': 'shop-name'}).text
+            print(shop_name)
+            address = item.find('div', {'class': 'shop-address'}).text
+            print(address)
+            phone = item.find('div', {'class': 'shop-phone'}).text
+            print(phone)
+            working_time = item.find('div', {'class': 'shop-weekends'}).text
+            print(working_time)
+            working_days = item.find('div', {'class': 'shop-work-time'}).text
+            print(working_days)
+            
+            latitude = str(item).partition('data-shop-latitude="')
+            print(latitude[2].split('" ')[0])
+            longitude = str(item).partition('data-shop-longitude="')
+            print(longitude[2].split('" ')[0])
+            
     else:
         print(f'No response from the site { url }')
 
