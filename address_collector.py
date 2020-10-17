f1669093b411ea8348f40546a78a10c7afc8cd1f
Vika Ratomskaya <ratom_v@mail.ru>
2020-10-17 19:58:08 +0300

    Added save to file json

--- address_collector.py
+++ address_collector.py
@@ -1,51 +1,51 @@
 import requests
 from bs4 import BeautifulSoup
 import os
+import json
 
 
 site1 = 'https://www.mebelshara.ru/contacts'
 site2 = 'https://www.tui.ru/offices/'
 
 
-def create_report(file_name, value):
+def create_report(file_name, data):
     os.makedirs('.\\json\\', exist_ok=True)
     folder_logs = os.path.abspath('.\\json\\' + file_name)
     with open(os.path.abspath(folder_logs), 'w') as fl:
-        for i in value:
-            fl.write(str(i))
-        fl.write('\n')
+        for i in data:
+            json.dump(i, fl, ensure_ascii=False, sort_keys=True)
     return fl
 
 
 def collector(url):
-    new_data = []
+    result = []
     s = requests.get(url)
     if (s.status_code == 200):
         soup = BeautifulSoup(s.text)
         group = soup.findAll('div', {'class': 'city-item'})
         #print(city)
-        create_report('json.txt', group)
         for item in group:
 
-            # getting movie_id
             city = item.find('div', {'class': 'expand-block-header js-ex-btn'}).find('h4').text
-            print(city)
             shop_name = item.find('div', {'class': 'shop-name'}).text
-            print(shop_name)
             address = item.find('div', {'class': 'shop-address'}).text
-            print(address)
             phone = item.find('div', {'class': 'shop-phone'}).text
-            print(phone)
             working_time = item.find('div', {'class': 'shop-weekends'}).text
-            print(working_time)
             working_days = item.find('div', {'class': 'shop-work-time'}).text
-            print(working_days)
             
             latitude = str(item).partition('data-shop-latitude="')
-            print(latitude[2].split('" ')[0])
+            latitude = latitude[2].split('" ')[0]
             longitude = str(item).partition('data-shop-longitude="')
-            print(longitude[2].split('" ')[0])
-            
+            longitude = longitude[2].split('" ')[0]
+
+            result.append({
+                'address': f'{ city }, { address }',
+                'latlon': [latitude, longitude],
+                'name': shop_name,
+                'phones': [phone],
+                'working_hours': [working_days, working_time]
+            })
+        create_report('json.txt', result)  
     else:
         print(f'No response from the site { url }')
 
